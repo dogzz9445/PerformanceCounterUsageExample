c508507ea3cc56ef0bc4df39860e4b49a4ebca93
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2021-10-25 21:47:31 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerformanceCounterUsageExample
+{
+    class Program
+    {
+        private static PerformanceCounter cpuCounter = 
+            new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        
+        private static PerformanceCounter memmoryCounter = 
+            new PerformanceCounter("Memory", "% Committed Bytes in Use");
+
+        private static PerformanceCounter networkCounter =
+            new PerformanceCounter("Network Interface", "Bytes Received/sec", "Bytes Sent/sec");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", cpuCounter.NextValue().ToString());
+            Console.ReadLine();
+        }
+    }
+}
